36ae0d09c4537a10cd07914e30ecbcd2b7e740b5
mubarak <mubarakcali24@gmail.com>
2023-07-23 18:58:45 -0700

    saving books

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,7 +31,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(bookInput: BookInput!): User
+    saveBook(bookData: BookInput!): User
     deleteBook(bookId: String!): User
   }
 
