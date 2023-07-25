f38cd2deb4bef784dae9a5735680fcb42564e876
mubarak <mubarakcali24@gmail.com>
2023-07-24 17:37:04 -0700

    deployed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search-engine');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Book-search-engine');
 
 module.exports = mongoose.connection;
